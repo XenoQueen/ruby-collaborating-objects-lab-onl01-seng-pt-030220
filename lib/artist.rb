46af16054cb206167c7e8034b852e376c733830b
Maika Kailani <queenmab051@gmail.com>
2020-03-28 23:14:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,13 +12,13 @@ class Artist
   def self.all
     @@all
   end
-
-  #def songs
-    #Song.all.select { |song| song.artist == self }
-  #end
-
+  
   def add_song(song)
     song.artist = self
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self }
   end
 
   def self.find_or_create_by_name(name)
